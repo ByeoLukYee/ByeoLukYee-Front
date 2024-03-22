739b6703aabe892aec5b1f94844cb622c0e16c5b
boram065 <boram4784@gmail.com>
2024-03-23 01:20:32 +0900

    홈 화면 구현(메뉴바 제외)

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login';
 import Join from './pages/join';
+import Home from './pages/home';
 
 ReactDOM.render(
     <Router>
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/join" element={<Join />} />
+            <Route path="/home" element={<Home />} />
         </Routes>
     </Router>,
     document.getElementById('root')
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../components/home/home'
+
+function homePage() {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
+
+export default homePage;
