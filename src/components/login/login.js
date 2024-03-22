e01052776af1a98dd8eb0fbad32fc45771d8dba6
boram065 <boram4784@gmail.com>
2024-03-22 21:52:05 +0900

    로그인 클래스명 수정

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,5 @@
 import loginStyles from '../../styles/login/login.module.css'
-import styles from '../../styles/common/styles.module.css';
+import styles from '../../styles/common/styles.css';
 
 function Login() {
     return (
@@ -10,19 +10,19 @@ function Login() {
             </div>
             <form className={loginStyles['formContainer']}>
                 <div>
-                    <p className={styles['textStyles']}>이메일</p> {/* SUIT Medium 글꼴(input도) */}
-                    <input type="text" placeholder='학교 이메일을 작성하시오.' className={styles['inputStyles']}/>
+                    <p className={loginStyles['textStyle']}>이메일</p> {/* SUIT Medium 글꼴(input도) */}
+                    <input type="text" placeholder='학교 이메일을 입력하시오.' className={loginStyles['inputStyles']}/>
                 </div>
                 <div style={{marginBottom: '15%'}}>
-                    <p className={styles['textStyles']}>비밀번호</p> {/* SUIT Medium 글꼴(input도) */}
-                    <input type='text' placeholder='비밀번호를 입력하시오.' className={styles['inputStyles']}/>
+                    <p className={loginStyles['textStyle']}>비밀번호</p> {/* SUIT Medium 글꼴(input도) */}
+                    <input type='text' placeholder='비밀번호를 입력하시오.' className={loginStyles['inputStyles']}/>
                 </div>
             </form>
             <div className={loginStyles['buttonContainer']}>
-                <button className={styles['buttonStyles']}>로그인</button> {/* SUIT Medium 글꼴 */}
+                <button className={loginStyles['buttonStyles']}>로그인</button> {/* SUIT Medium 글꼴 */}
                 <button className={loginStyles['googleButton']}> <img src={process.env.PUBLIC_URL + '/images/GoogleImg.png'} /> 구글로 로그인</button>
             </div>
-            <div className={styles['miniTextStyles']}>
+            <div className={loginStyles['miniTextStyles']}>
                 <p>계정이 없다면?</p>
                 <p>회원가입</p>
             </div>
