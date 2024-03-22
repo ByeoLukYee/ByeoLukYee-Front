5aab6cba2a55c9056360cadf74d340ffce0b0a5a
boram065 <boram4784@gmail.com>
2024-03-22 21:51:14 +0900

    titlebar 구현

---
+++ src/components/common/titleBar.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/common/titleBar.module.css';
+
+function titleBar() {
+    return(
+        <div className={styles['top']}></div>
+    )
+}
+
+export default titleBar;
