1556f7bf2a2bd255a0b056e8e72b24dc475009ee
boram065 <boram4784@gmail.com>
2024-03-23 00:50:39 +0900

    검색창 구현

---
+++ src/components/common/searchInput.js
@@ -0,0 +1,12 @@
+import searchInputStyle from '../../styles/common/searchInput.module.css';
+
+function searchInput() {
+    return(
+        <div className={searchInputStyle['searchDiv']}>
+            <input type='text' placeholder='관심있는 상품을 검색해보세요!' className={searchInputStyle['searchInput']} />
+            <div className={searchInputStyle['searchImg']}> <img src={process.env.PUBLIC_URL + '/images/circum_search.png'} className={searchInputStyle['imageStyle']}/> </div>
+        </div>
+    )
+}
+
+export default searchInput;
