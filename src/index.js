22b710c2b4078e01c2ef4ce10e05a4ec616a7f5a
boram065 <boram4784@gmail.com>
2024-03-22 21:51:03 +0900

    회원가입

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login';
+import Join from './pages/join';
 
 ReactDOM.render(
     <Router>
         <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/join" element={<Join />} />
         </Routes>
     </Router>,
     document.getElementById('root')
