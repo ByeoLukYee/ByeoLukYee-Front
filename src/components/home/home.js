2b4239ff41aae1bcae84d2784bb1339b311bc98a
boram065 <boram4784@gmail.com>
2024-03-23 19:20:58 +0900

    재사용할 component 분리

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,7 @@ import homeStyle from '../../styles/home/home.module.css';
 import TitleBar from '../common/titleBar';
 import SearchInput from '../common/searchInput';
 import MenuBar from '../common/menuBar';
+import SellDiv from '../common/sellDiv';
 
 import { GoHeart } from "react-icons/go";
 import { VscEye } from "react-icons/vsc";
@@ -44,26 +45,7 @@ function Home() {
                 <div className={homeStyle['newContainer']}>
                     <p className={homeStyle['lookUserP']}>따끈따끈 최신 글</p>
                     <div className={homeStyle['newScrollContainer']}>
-                        <div className={homeStyle['newBoxContainer']}>
-                            <div className={homeStyle['newLookImgStyle']}> <img src={process.env.PUBLIC_URL+'/images/exampleImg.png'}/> </div>
-                            <div className={homeStyle['newTextContainer']}>
-                                <p>#판매중</p>
-                                <p>제목입니다.</p>
-                                <p style={{top: "5%"}}>40,000원</p>
-
-                                <div className={homeStyle['newVectorDiv']}>
-                                    {/* 몇명 봤는지, 누가 찜했는지 */}
-                                    <div className={homeStyle['newEyesVectorDiv']}>
-                                        <VscEye className={homeStyle['newEyesVector']}/>
-                                        <p>0</p>
-                                    </div>
-                                    <div className={homeStyle['newHeartVectorDiv']}>
-                                        <GoHeart className={homeStyle['newHeartVector']}/>
-                                        <p>0</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <SellDiv />
                     </div>
                 </div>
                 {/* 메뉴바 component */}
