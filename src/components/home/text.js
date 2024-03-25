c8fa8859f2d2ff9235a2e69007dfcf60e456a71f
boram065 <boram4784@gmail.com>
2024-03-25 15:03:53 +0900

    컴포넌트 분리

---
+++ src/components/home/text.js
@@ -0,0 +1,11 @@
+import textStyle from '../../styles/home/home.module.css';
+
+function text() {
+    return(
+        <>
+            <p className={textStyle['lookUserP']}>사용자님이 최근 본 글</p>
+        </>
+    )
+}
+
+export default text;
