26f1b45165d272ec95d00e7b8ee369f22a44fd74
boram065 <w2216@e-mirim.hs.kr>
2024-03-27 20:36:14 +0900

    프로필 프론트 구현

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,6 +3,14 @@ import profileStyle from '../../styles/profile/profile.module.css';
 
 import TitleBar from '../common/titleBar';
 
+import { BsPencilSquare } from "react-icons/bs";
+import { PiChatThin } from "react-icons/pi";
+import { BsCardText } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
+import { BsCodeSlash } from "react-icons/bs";
+import { LuBookMarked } from "react-icons/lu";
+import { BsBoxArrowInRight } from "react-icons/bs";
+
 function Profile() {
     return(
        <div className={profileStyle['divContainer']}>
@@ -18,10 +26,28 @@ function Profile() {
                 </div>
             </div>
             <hr />
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className={profileStyle['informationDiv']}> 
+                {/* 내 정보 */}
+                <div className={profileStyle['selectorDiv1']}>
+                    <p>내 정보</p>
+                    <div className={profileStyle['iconDiv']}> <BsPencilSquare style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>프로필 수정하기</p> </div>
+                    <div className={profileStyle['iconDiv']}> <PiChatThin style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>채팅 관리하기</p> </div>
+                    <div className={profileStyle['iconDiv']}> <BsCardText style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>작성글 관리</p> </div>
+                    <div className={profileStyle['iconDiv']}> <BsEnvelope style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>이메일 인증하기</p> </div>
+                </div>
+                <hr />
+
+                <div className={profileStyle['selectorDiv2']}>
+                    <p>도움말</p>
+                    <div className={profileStyle['iconDiv']}> <BsCodeSlash style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>오픈소스라이센스</p> </div>
+                    <div className={profileStyle['iconDiv']}> <LuBookMarked style={{fontSize: '1.1rem', color: 'var(--gray-300)'}} /> <p>서비스 이용약관</p> </div>  
+                </div>
+                <hr />
+
+                <div className={profileStyle['selectorDiv2']}>
+                    <p>계정</p>
+                    <div className={profileStyle['iconDiv']}> <BsBoxArrowInRight style={{fontSize: '1.2rem', color: 'var(--gray-300)'}} /> <p>로그아웃</p> </div>
+                </div>
             </div>
        </div>
     )
