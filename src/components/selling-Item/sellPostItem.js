acc75eb45b960137acbea5b60aec55891f4c0cae
boram065 <w2216@e-mirim.hs.kr>
2024-04-03 20:03:27 +0900

    [Design] div 구조 변경

--- src/components/selling-Item/sellPostItem.js
+++ src/components/selling-Item/sellPostItem.js
@@ -10,29 +10,35 @@ function SellDiv({ status, price, title }) {
   return (
     <>
       <Link to="/sellDetail" style={{ textDecoration: "none", color: "black" }}>
-        <div className={sellStyle["newBoxContainer"]}>
-          <div className={sellStyle["newLookImgStyle"]}>
+        <div className={sellStyle["boxContainer"]}>
+
+          <div className={sellStyle["lookImgStyle"]}>
             <img
               src={process.env.PUBLIC_URL + "/images/exampleImg.png"}
               alt="example"
             />
           </div>
-          <div className={sellStyle["newTextContainer"]}>
-            <p># {status}</p>
-            <p>{title}</p>
-            <p style={{ top: "5%" }}>{price}원</p>
-            <div className={sellStyle["newVectorDiv"]}>
+
+          <div className={sellStyle['contextContainer']}>
+            <div className={sellStyle["textContainer"]}>
+              <p># {status}</p>
+              <p>{title}</p>
+              <p style={{ top: "5%" }}>{price}원</p>
+            </div>
+
+            <div className={sellStyle["vectorDiv"]}>
               {/* 몇명 봤는지, 누가 찜했는지 */}
-              <div className={sellStyle["newEyesVectorDiv"]}>
-                <VscEye className={sellStyle["newEyesVector"]} />
-                <p>{price}</p>
+              <div className={sellStyle["eyesVectorDiv"]}>
+                <VscEye className={sellStyle["eyesVector"]} />
+                <p>0</p>
               </div>
-              <div className={sellStyle["newHeartVectorDiv"]}>
-                <GoHeart className={sellStyle["newHeartVector"]} />
+              <div className={sellStyle["heartVectorDiv"]}>
+                <GoHeart className={sellStyle["heartVector"]} />
                 <p>0</p>
               </div>
             </div>
           </div>
+
         </div>
       </Link>
     </>
