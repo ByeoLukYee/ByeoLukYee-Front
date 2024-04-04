39ce71b35890b71d4fd19a362c05cd7abe830ded
boram065 <boram4784@gmail.com>
2024-04-04 19:57:03 +0900

    로그인 화면 만드는 중

--- src/components/common/searchInput.js
+++ src/components/common/searchInput.js
@@ -4,8 +4,7 @@ import searchInputStyle from '../../styles/common/searchInput.module.css';
 function SearchInput() {
     return(
         <div className={searchInputStyle['searchDiv']}>
-            <input type='text' placeholder='관심있는 상품을 검색해보세요!' className={searchInputStyle['searchInput']} />
-            <div className={searchInputStyle['searchImg']}> <img src={process.env.PUBLIC_URL + '/images/circum_search.png'} className={searchInputStyle['imageStyle']}/> </div>
+            <input type='text' placeholder='찾는 상품을 검색해 보세요!' className={searchInputStyle['searchInput']} />
         </div>
     )
 }
