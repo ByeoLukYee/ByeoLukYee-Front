e89f2b593238564801eda066baf2db0c4fac812d
boram065 <boram4784@gmail.com>
2024-04-05 22:07:58 +0900

    [feat] 새로운 기능 추가

--- src/components/join/join.js
+++ src/components/join/join.js
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 
 import joinStyles from '../../styles/join/join.module.css';
 import styles from '../../styles/common/styles.css';
-import TitleBar from '../common/titleBar';
+
+import Nav from '../common/nav';
+import Footer from '../common/footer';
 
 function Join() {
     // 로그인 화면으로 넘어가기
@@ -14,45 +16,46 @@ function Join() {
 
     return(
         <div className={joinStyles['divContainer']}>
-            <TitleBar />
-            <div className={joinStyles['DivStyle']}>
-                <p>회원가입</p>
-            </div>
-            <div className={joinStyles['formContainer']}>
-                <div>
-                    <p className={joinStyles['textStyle']}>이름</p>
-                    <input id="txtName" type="text" placeholder='이름을 입력하시오.' className={joinStyles['inputStyles']}/>
-                </div>
-                <div>
-                    <p className={joinStyles['textStyle']}>이메일</p>
-                    <input id="txtEmail" type="text" placeholder='학교 이메일을 입력하시오.' className={joinStyles['inputStyles']}/>
-                </div>
-                <div>
-                    <p className={joinStyles['textStyle']}>비밀번호</p>
-                    <input id="txtPw" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']}/>
-                </div>
-                <div>
-                    <p className={joinStyles['textStyle']}>비밀번호 검사</p>
-                    <input id="txtRePw" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']}/>
-                </div>
-                <div>
-                    <p className={joinStyles['textStyle']}>학생 정보</p>
-                    <div className={joinStyles['studentDivStyle']}>
-                        <div> <input id='txtGrade' type='text' placeholder='학년' className={joinStyles['studentInputStyles']}/> </div>
-                        <div> <input id='txtClass' type='text' placeholder='반' className={joinStyles['studentInputStyles']}/> </div>
-                        <div> <input id='txtNumber' type='text' placeholder='번호' className={joinStyles['studentInputStyles']}/> </div>
-                    </div>
-                </div>
-                <div className={joinStyles['buttontextContainer']}>
-                    <div className={joinStyles['buttonContainer']}>
-                        <button id='btnJoin' className={joinStyles['buttonStyles']}>회원가입</button>
+            <Nav />
+            <div className={joinStyles['formDiv']}>
+                <div className={joinStyles['joinText']}> <p>회원가입</p> </div>
+                <form className={joinStyles['formContainer']}>
+                    <div className={joinStyles['inputContainer']}>
+                        <div>
+                            <p className={joinStyles['textStyle']}>이름</p>
+                            <input id="txtName" type="text" placeholder='이름을 입력하시오.' className={joinStyles['inputStyles']}/>
+                        </div>
+                        <div>
+                            <p className={joinStyles['textStyle']}>이메일</p>
+                            <input id="txtEmail" type="text" placeholder='학교 이메일을 입력하시오.' className={joinStyles['inputStyles']}/>
+                        </div>
+                        <div>
+                            <p className={joinStyles['textStyle']}>비밀번호</p>
+                            <input id="txtPw" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']}/>
+                        </div>
+                        <div>
+                            <p className={joinStyles['textStyle']}>비밀번호 검사</p>
+                            <input id="txtRePw" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']}/>
+                        </div>
+                        <div>
+                            <p className={joinStyles['textStyle']}>학생 정보</p>
+                            <div className={joinStyles['studentDivStyle']}>
+                                <div> <input id='txtGrade' type='text' placeholder='학년' className={joinStyles['studentInputStyles']}/> </div>
+                                <div> <input id='txtClass' type='text' placeholder='반' className={joinStyles['studentInputStyles']}/> </div>
+                                <div> <input id='txtNumber' type='text' placeholder='번호' className={joinStyles['studentInputStyles']}/> </div>
+                            </div>
+                        </div>
+                        <div className={joinStyles['buttonContainer']}>
+                            <button id='btnJoin' className={joinStyles['buttonStyles']}>회원가입</button>
+                        </div>
                     </div>
                     <div className={joinStyles['miniTextStyles']}>
                         <p>이미 계정이 있다면?</p>
                         <p onClick={() => TextClick('/')}>로그인하기</p>
                     </div>
-                </div>
+                </form>
             </div>
+            <Footer />
         </div>
     )
 }
