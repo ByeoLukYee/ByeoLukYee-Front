62899a4ead7fd289bf295e001abb6eeb8e73d776
boram065 <boram4784@gmail.com>
2024-04-06 15:27:06 +0900

    [Feat] 새로운 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login';
 import Join from './pages/join';
-import Page from './pages/page';
-
-import SellDetail from './pages/sellDetail';
-import BuyDetail from './pages/buyDetail';
-import AuctionParticipation from './pages/auctionParticipation';
 import Home from './components/home/home';
+import SellingPosts from './components/selling-Item/selling-posts';
 
 function App() {
     return(
@@ -17,10 +13,7 @@ function App() {
                 <Route path="/signin" element={<Login />} />
                 <Route path="/signup" element={<Join />} />
                 <Route path="/" element={<Home />} />
-                
-                <Route path="/sellDetail" element={<SellDetail />} />
-                <Route path="/buyDetail" element={<BuyDetail />} />
-                <Route path="/auctionParticipation" element={<AuctionParticipation />} />
+                <Route path='/selling-posts' element={<SellingPosts />}/>
             </Routes>
         </Router>
     )
