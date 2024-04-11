be7c8abd17c26c7673145442b1941f2e52049ccf
boram065 <boram4784@gmail.com>
2024-04-11 18:46:05 +0900

    [refactor] 컴포넌트 분리

--- src/components/selling-posts/SellingPostIdInfo.js
+++ src/components/selling-posts/SellingPostIdInfo.js
@@ -1,7 +1,24 @@
 import '../../styles/common/Styles.css';
+import styles from '../../styles/selling-posts/SellingPostIdInfo.module.css';
 
 function SellingPostIdInfo() {
-
+    return (
+        <>
+            <div className={styles['container']}>
+                <div className={styles['textContainer']}>
+                    <p>#경매중</p>
+                    <div className={styles['GoodSee']}>
+                        <p>좋아요 4</p>
+                        <p>조회 20</p>
+                    </div>                    
+                    <div className={styles['place']}>
+                        <p>희망 거래 장소</p>
+                        <p>신림역 3번출구</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SellingPostIdInfo;
