2774d8235d9634a611163faba11bf0a85fa2986b
boram065 <boram4784@gmail.com>
2024-04-12 15:07:49 +0900

    [feat] 클릭 시 색 변경

--- src/components/comment/CommentItem.js
+++ src/components/comment/CommentItem.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/comment/CommentItem.module.css';
 
 function CommentItem() {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
     return (
         <>
-            <div className={styles['container']}>
+            <div className={`${styles['container']} ${clicked ? styles['clickedContainer'] : ''}`} onClick={handleClick}>
                 <div className={styles['top']}>
                     <p>title</p>
                     <div className={styles['info']}>
@@ -12,7 +20,7 @@ function CommentItem() {
                         <p>place</p>
                     </div>
                 </div>
-                <div  className={styles['hr']}> <hr /> </div>
+                <div className={`${styles['hr']} ${clicked ? styles['clickedHr'] : styles['defalutHr']}`}> <hr /> </div>
                 <div className={styles['bottom']}>
                     <p>content</p>
                 </div>
