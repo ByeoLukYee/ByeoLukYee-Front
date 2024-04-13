3f551ea663be153d1632e0d1d632616db1db4232
boram065 <boram4784@gmail.com>
2024-04-13 10:31:23 +0900

    [feat] 프로필 화면 구현

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/profile/Profile.module.css';
 
@@ -5,21 +7,34 @@ import Header from '../common/Header';
 import ProfileList from './ProfileList';
 import ProfileInfo from './ProfileInfo';
 import ProfileSave from './ProfileSave';
+import ProfileWriteManagement from './PostWriteManagement';
 import Footer from '../common/Footer';
 
 function Profile() {
+    const [showSelectComponent, setShowSelectComponent] = useState('')
+
+    const showComponent = () => {
+        switch (showSelectComponent) {
+            case 'writeManagement':
+                return <ProfileWriteManagement />;
+            default:
+                return <ProfileSave />;
+        }
+    };
+
     return( 
        <div className={styles['container']}>
             <Header />
             
             <div className={styles['divContainer']}>
-                <ProfileList />
+                <ProfileList showSelectComponent={setShowSelectComponent} />
 
                 <div className={styles['profile']}>
                     <ProfileInfo />
                     
                     <div className={styles['lists']}>
-                        <ProfileSave />
+                        {/* 바뀌는 화면 */}
+                        {showComponent()}
                     </div>
                 </div>
             </div>
