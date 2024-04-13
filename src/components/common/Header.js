79daff2026abf2c54f9cc4f6eee6fcff2a7541c6
boram065 <boram4784@gmail.com>
2024-04-13 13:32:57 +0900

    [refactor] 코드 리펙토링

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import '../../styles/common/Styles.css';
-import headerStyle from '../../styles/common/Header.module.css';
+import styles from '../../styles/common/Header.module.css';
 
 import SearchInput from './SearchInput';
 
 function Header({ onTitleClick }) {
     return (
         <>
-            <nav className={headerStyle['nav']}>
-                <div className={headerStyle['container1']}>
-                    <Link to='/' style={{ textDecoration: "none", color: 'black' }}> <div className={headerStyle['titleDiv']} onClick={onTitleClick}> <p>벼룩이</p> </div> </Link>
+            <nav className={styles['nav']}>
+                <div className={styles['container1']}>
+                    <Link to='/' className={styles['link']}> <div className={styles['titleDiv']} onClick={onTitleClick}> <p>벼룩이</p> </div> </Link>
                     <div> <SearchInput /> </div>
                 </div>
-                <div className={headerStyle['optionDiv']}>
-                    <Link to='/buying-posts/upload' style={{ textDecoration: "none", color: 'black' }}> <p>팝니다 글쓰기</p> </Link>
-                    <Link to='/selling-posts/upload' style={{ textDecoration: "none", color: 'black' }}> <p>삽니다 글쓰기</p> </Link>
-                    <p>채팅</p>
-                    <Link to='/profile' style={{ textDecoration: "none", color: 'black' }}> <p>마이페이지</p> </Link>
+                <div className={styles['optionDiv']}>
+                    <Link to='/buying-posts/upload' className={styles['link']}> <p>팝니다 글쓰기</p> </Link>
+                    <Link to='/selling-posts/upload' className={styles['link']}> <p>삽니다 글쓰기</p> </Link>
+                    <Link to='/chatting' className={styles['link']}> <p>채팅</p> </Link>
+                    <Link to='/profile' className={styles['link']}> <p>마이페이지</p> </Link>
                 </div>
             </nav>
             <div style={{textAlign: 'center'}}> <hr /> </div>
