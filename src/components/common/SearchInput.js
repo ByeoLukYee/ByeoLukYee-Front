9efc22fd93080b07cad8b663976d5a5e0eb051fd
boram065 <boram4784@gmail.com>
2024-04-23 19:32:50 +0900

    comment: 서버 연결 코멘트 추가

--- src/components/common/SearchInput.js
+++ src/components/common/SearchInput.js
@@ -1,10 +1,40 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import '../../styles/common/Styles.css';
 import searchInputStyle from '../../styles/common/SearchInput.module.css';
 
 function SearchInput() {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const navigate = useNavigate();
+
+    const handleInputChange = (event) => {
+        setSearchKeyword(event.target.value);
+    };
+
+    // 엔터 키 입력 시 검색 실행
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            search();
+        }
+    };
+
+    const search = () => {
+        if (searchKeyword.trim() !== '') {
+            navigate(`/search?keyword=${encodeURIComponent(searchKeyword)}`);
+        }
+    };
+
     return(
         <div className={searchInputStyle['searchDiv']}>
-            <input type='text' placeholder='찾는 상품을 검색해 보세요!' className={searchInputStyle['searchInput']} />
+            <input 
+                type='text' 
+                placeholder='찾는 상품을 검색해 보세요!' 
+                className={searchInputStyle['searchInput']} 
+                value={searchKeyword}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+            />
         </div>
     )
 }
