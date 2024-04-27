6c1e0c27715e408574f6e392e8a7ba48b522ce41
boram065 <boram4784@gmail.com>
2024-04-27 21:51:32 +0900

    Feat: 프로필 정보 데이터 불러오기

--- src/components/profile/ProfileCommentList.js
+++ src/components/profile/ProfileCommentList.js
@@ -1,16 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { HOST } from '../../config/Config';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/profile/ProfileCommentList.module.css';
 
 import CommentItem from '../comment/CommentItem';
 
 function ProfileCommentList() {
+    // 내가 쓴 댓글 조회하기
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`${HOST}/selling-comments/1`);
+                if(response.status === 200) {
+                    const filteredData = response.data.filter(item => item.user.id === 1);
+                    setData(filteredData);
+                }
+            } catch(error) {
+                console.error("댓글 불러오기 실패 : ", error);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <>
-            <div className={styles['comment-container']}>
-                <div className={styles['comment-item']}>
-                    <CommentItem />
+            {data.length > 0 &&
+                <div className={styles['comment-container']}>
+                    <div className={styles['comment-item']}>
+                        <CommentItem data={data} />
+                    </div>
                 </div>
-            </div>
+            }
+
+            {data.length <= 0 &&
+                <p>경매 댓글 없습니다.</p>
+            }
         </>
     )
 }
--- src/components/profile/ProfileInfo.js
+++ src/components/profile/ProfileInfo.js
@@ -1,3 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { HOST } from '../../config/Config';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/profile/ProfileInfo.module.css';
 import ProfileActivityInfo from './ProfileActivityInfo';
@@ -5,10 +9,27 @@ import ProfileActivityInfo from './ProfileActivityInfo';
 import ProfileMyInfo from './ProfileMyInfo';
 
 function ProfileInfo() {
+    const [data, setData] = useState([]);
+    let id = 1;
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`${HOST}/users/${id}`);
+                if (response.status === 200) {
+                    setData(response.data);
+                }
+            } catch(error) {
+                console.log("유저 데이터 GET 실패 : ", error);
+            }
+        }
+        fetchData();
+    }, []);
+    console.log(data);
+
     return (
         <>
             <div className={styles['container']}>
-                <ProfileMyInfo />
+                <ProfileMyInfo data={data}/>
                 <ProfileActivityInfo />
             </div>
         </>
