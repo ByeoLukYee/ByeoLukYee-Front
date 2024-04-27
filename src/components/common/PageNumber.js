62122b0fa96010d2d39a4a0db59b1afdd35fffe6
boram065 <boram4784@gmail.com>
2024-04-27 15:28:49 +0900

    Feat: 페이지 번호 자동 생성

--- src/components/common/PageNumber.js
+++ src/components/common/PageNumber.js
@@ -3,17 +3,31 @@ import React, { useState } from 'react';
 import '../../styles/common/Styles.css';
 import styles from '../../styles/common/PageNumber.module.css';
 
-function PageNumber() {
-    const [selected, setSelected] = useState('1');
-
+function PageNumber({ totalPosts, postsPerPage, onPageChange }) {
+    const [selected, setSelected] = useState(1);
+    const pageCount = Math.ceil(totalPosts / postsPerPage);
     const handleSelectedClick = (number) => {
         setSelected(number);
+        onPageChange(number);
     };
+
+    const pageNumbers = [];
+    for (let i = 1; i <= pageCount; i++) {
+        pageNumbers.push(i);
+    }
+
+    let pathName = window.location.pathname;
 
     return (
         <>
-            <div className={styles['selectDiv']}>
-                <div className={styles[selected === '1' ? 'selected' : '']} onClick={() => handleSelectedClick('1')}> <p>1</p> </div>
+            <div className={pathName === '/selling-posts' ? styles['sellingSelectDiv'] : (pathName === '/buying-posts' ? styles['buyingSelectDiv'] : '')}>
+                <div className={styles['div']}>
+                    {pageNumbers.map(number => (
+                        <div key={number} className={number === selected ? styles['selected'] : ''} onClick={() => handleSelectedClick(number)}>
+                            <p>{number}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
