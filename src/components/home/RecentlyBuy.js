c6dc21176fdb71d7f298ba7abac4c8c651c1994e
boram065 <boram4784@gmail.com>
2024-04-27 14:25:02 +0900

    Feat: 최신 팝니다/삽니다 서버 연결

--- src/components/home/RecentlyBuy.js
+++ src/components/home/RecentlyBuy.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { HOST } from '../../config/Config';
 import { Link } from 'react-router-dom';
 
 import '../../styles/common/Styles.css';
@@ -7,11 +9,26 @@ import styles from '../../styles/home/RecentlyBuy.module.css';
 import BuyPostItemList from "../buying-Item/BuyPostItemList";
 
 function RecentlyBuy() {
+    const [latestPosts, setLatestPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchLatestPosts() {
+            try {
+                const response = await axios.get(`${HOST}/buying-posts?limit=4&sortBy=createdAt:desc`);
+                setLatestPosts(response.data);
+            } catch(error) {
+                console.error("요청 실패 : ", error);
+            }
+        }
+
+        fetchLatestPosts();
+    }, []);
+
     return (
         <>
             <div className={styles['buyTextDiv']}> <p>따끈따근 최신 삽니다 글</p> </div>
             <div className={styles['buyContainer']}>
-                {/* <BuyPostItemList /> */}
+                <BuyPostItemList data={latestPosts}/>
             </div>
 
             <div className={styles['buyMoreDiv']}>
