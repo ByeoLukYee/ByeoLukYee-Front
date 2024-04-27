62001300dcd9bff68dc4e91db9214a4b0cac14bb
boram065 <boram4784@gmail.com>
2024-04-27 21:50:40 +0900

    Feat: 댓글 데이터 불러오기

--- src/components/comment/CommentList.js
+++ src/components/comment/CommentList.js
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { HOST } from '../../config/Config';
+import { useParams } from 'react-router';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/comment/CommentList.module.css';
 
@@ -6,12 +11,37 @@ import CommentItem from './CommentItem';
 function CommentList() {
     // /selling-comments에서 GET하기
     // title, price, location, desc CommentItem에 전달하기
+    const [data, setData] = useState([]);
+    const { id } = useParams();
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`${HOST}/selling-comments/${id}`);
+                if(response.status === 200) {
+                    setData(response.data);
+                }
+            } catch(error) {
+                console.error("댓글 불러오기 실패 : ", error);
+            }
+        }
+        fetchData();
+    }, [id]);
+
     return (
         <>
             <div className={styles['comment-container']}>
-                <div className={styles['comment-item']}>
-                    <CommentItem />
-                </div>
+                {data.length > 0 && (
+                    data.map((comment, index) => (
+                        <div key={index} className={styles['comment-item']}>
+                            <CommentItem data={comment}/>
+                        </div>
+                    ))
+                )}
+                {data.length <= 0 && (
+                    <div className={styles['no-comment']}>
+                        <p>댓글이 없습니다.</p>
+                    </div>
+                )}
             </div>
         </>
     )
