69a1b4309e619c8f6cfb17d6e69b483fea676628
boram065 <boram4784@gmail.com>
2024-04-27 21:51:54 +0900

    Feat: 컴포넌트 불러오기

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,30 +1,33 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router';
+
 import '../../styles/common/Styles.css';
 import styles from '../../styles/search/Search.module.css';
 
 import Header from '../common/Header';
 import PageNumber from '../common/PageNumber';
+import SearchBuyingResult from './SearchBuyingResult';
+import SearchSellingResult from './SearchSellingResult';
 import Footer from '../common/Footer';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 function Search() {
-    const [selected, setSelected] = useState('');
+    const [selected, setSelected] = useState('sell');
 
     const clickOption = (text) => {
         setSelected(text);
     }
 
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const searchKeyword = searchParams.get('keyword');
     const rederContent = () => {
         // 팝니다, 삽니다 게시글 검색 분리
+        switch (selected) {
+            case 'sell' : return <SearchSellingResult keyword={searchKeyword} />;
+            case 'buy' : return <SearchBuyingResult keyword={searchKeyword} />;
+        }
     }
 
-    // const location = useLocation();
-    // const searchParams = new URLSearchParams(location.search);
-    // const searchKeyword = searchParams.get('keyword');
-    // {searchKeyword}
-
-    // /selling-posts와 /buying-posts에 저장된 게시글 데이터를 GET하여
-    // 내가 검색창에 입력 값과 일치하거나 포함되어있는 데이터가 있으면 그 데이터만 가져와서 화면에 보여주기
 
     return (
         <>
