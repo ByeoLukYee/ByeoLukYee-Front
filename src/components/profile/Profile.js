f05c591252aaa97c0b29830978839a96de9a241c
boram065 <boram4784@gmail.com>
2024-05-20 14:58:54 +0900

    Feat: 프로필 내가 쓴 팝니다, 삽니다 글, 댓글 서버 연결

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { HOST } from '../../config/Config';
+import axios from 'axios';
 
 import '../../styles/common/Styles.css';
 import styles from '../../styles/profile/Profile.module.css';
@@ -11,28 +13,78 @@ import ProfileWriteManagement from './PostWriteManagement';
 import Footer from '../common/Footer';
 
 function Profile() {
-    const [showSelectComponent, setShowSelectComponent] = useState('')
+    const [showSelectComponent, setShowSelectComponent] = useState('');
+    // 팝니다, 삽니다 게시글 get (내가 올린 것만)
+    // sellingPostData, buyingPostData, commentData ProfileWriteManagement 컴포넌트에 전달
+    // sellingPostData.length, buyingPostData.length, commentData.length ProfileInfo 컴포넌트에 전달
+    const [sellingPostData, setSellingPostData] = useState([]);
+    const [buyingPostData, setBuyingPostData] = useState([]);
+    const [commentData, setCommentData] = useState([]);
+    const userId = Number(localStorage.getItem("userId"));
+    useEffect(() => {
+        async function sellingData() {
+            try {
+                const response = await axios.get(`${HOST}/users/${userId}/selling-posts`);
+                if (response.status === 200) {
+                    setSellingPostData(response.data);
+                }
+            } catch (error) {
+                console.error("데이터 가져오기 실패: ", error);
+            }
+        };
+
+        async function buyingData() {
+            try {
+                const response = await axios.get(`${HOST}/users/${userId}/buying-posts`);
+                if (response.status === 200) {
+                    setBuyingPostData(response.data);
+                }
+            } catch (error) {
+                console.error("데이터 가져오기 실패: ", error);
+            }
+        };
+
+        async function commentData() {
+            try {
+                const response = await axios.get(`${HOST}/users/${userId}/selling-comments`);
+                if(response.status === 200) {
+                    setCommentData(response.data);
+                }
+            } catch(error) {
+                console.error("댓글 불러오기 실패 : ", error);
+            }
+        }
+
+
+        buyingData();
+        sellingData();
+        commentData();
+    }, []);
 
     const showComponent = () => {
         switch (showSelectComponent) {
             case 'writeManagement':
-                return <ProfileWriteManagement />;
+                return <ProfileWriteManagement sellingPostData={sellingPostData} buyingPostData={buyingPostData} commentData={commentData} />;
             default:
                 return <ProfileSave />;
         }
     };
 
-    const id = Number(localStorage.getItem("userId"));
 
     return( 
        <div className={styles['container']}>
             <Header />
             
             <div className={styles['divContainer']}>
-                <ProfileList showSelectComponent={setShowSelectComponent} userId={id} />
+                <ProfileList showSelectComponent={setShowSelectComponent} userId={userId} />
 
                 <div className={styles['profile']}>
-                    <ProfileInfo userId={id}/>
+                    <ProfileInfo 
+                        userId={userId} 
+                        sellingPostDataLength={sellingPostData.length} 
+                        buyingPostDataLength={buyingPostData.length} 
+                        commentDataLength={commentData.length}
+                    />
                     
                     <div className={styles['lists']}>
                         {/* 바뀌는 화면 */}
--- src/components/profile/ProfileCommentList.js
+++ src/components/profile/ProfileCommentList.js
@@ -1,42 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { HOST } from '../../config/Config';
-
 import '../../styles/common/Styles.css';
 import styles from '../../styles/profile/ProfileCommentList.module.css';
 
-import CommentItem from '../comment/CommentItem';
+import ProfileCommentItem from './ProfileCommentItem';
 
-function ProfileCommentList() {
-    // 내가 쓴 댓글 조회하기
-    let userId = Number(localStorage.getItem('userId'));
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get(`${HOST}/selling-comments`);
-                if(response.status === 200) {
-                    const filteredData = response.data.filter(item => item.user.id === userId);
-                    setData(filteredData);
-                }
-            } catch(error) {
-                console.error("댓글 불러오기 실패 : ", error);
-            }
-        }
-        fetchData();
-    }, []);
-
+function ProfileCommentList({ commentData }) {
     return (
         <>
-            {data.length > 0 &&
-                <div className={styles['comment-container']}>
-                    <div className={styles['comment-item']}>
-                        <CommentItem data={data} />
-                    </div>
-                </div>
-            }
+            <div className={styles['comment-container']}>
+                {
+                    commentData.length > 0 && commentData.map((item, index) => {
+                        return (
+                            <div className={styles['comment-item']} key={index}>
+                                <ProfileCommentItem commentData={item} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
-            {data.length <= 0 &&
+            {commentData.length <= 0 &&
                 <p>경매 댓글 없습니다.</p>
             }
         </>
