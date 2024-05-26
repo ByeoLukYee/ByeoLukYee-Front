47e6e29fef667c3cf7fe823bdaccceeaef7fa742
boram065 <boram4784@gmail.com>
2024-05-26 09:16:09 +0900

    Fix: 비밀번호가 일치하지 않을 경우, 이메일이 존재할 경우 에러메시지 띄움 해결

--- src/components/join/Join.js
+++ src/components/join/Join.js
@@ -9,6 +9,9 @@ import Header from '../common/Header';
 import Footer from '../common/Footer';
 
 function Join() {
+    const [fail, setFail] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
     // 로그인 화면으로 넘어가기
     const navigate = useNavigate();
     const TextClick = (path) => {
@@ -40,7 +43,9 @@ function Join() {
             if (!userData.grade || !userData.class || !userData.number) {
                 alert('학생 정보를 모두 입력하세요.');
                 return;
-            }
+            } else if(!validatePassword()) {
+                return;
+            }   
 
             let studentId = `${userData.grade}${userData.class}${userData.number}`;
             const response = await axios.post(`${process.env.REACT_APP_HOST}/users`, {
@@ -52,15 +57,18 @@ function Join() {
 
             if (response.status === 200) {
                 console.log('회원가입 성공');
+                setFail(false);
                 navigate('/signin');
             } else {
                 console.error('회원가입 실패');
+                setFail(true);
             }
         } catch (error) {
-            if (error.response.data.message === "이미 가입된 이메일입니다.") {
-                alert(error.response.data.message);
-            } else{
-                console.error('회원가입 요청 중 에러:', error);
+            setFail(true);
+            if (error.response.data.errorMessage === "이미 가입된 이메일입니다.") {
+                setErrorMessage(error.response.data.errorMessage);
+            } else {
+                console.error('서버 연결 실패', error);
             }
         }
     };
@@ -91,24 +99,25 @@ function Join() {
                 <div className={joinStyles['joinText']}> <p>회원가입</p> </div>
                 <form className={joinStyles['formContainer']} onSubmit={handleSubmit}>
                     <div className={joinStyles['inputContainer']}>
-                        <div>
+                        <div className={joinStyles['div']}>
                             <p className={joinStyles['textStyle']}>이름</p>
                             <input id="name" type="text" placeholder='이름을 입력하시오.' className={joinStyles['inputStyles']} onChange={handleChange}/>
                         </div>
-                        <div>
+                        <div className={joinStyles['div']}>
                             <p className={joinStyles['textStyle']}>이메일</p>
                             <input id="email" type="text" placeholder='학교 이메일을 입력하시오.' className={joinStyles['inputStyles']} onChange={handleChange}/>
+                            { fail && <p className={joinStyles['errorMessage']}>{errorMessage}</p> }
                         </div>
-                        <div>
+                        <div className={joinStyles['div']}>
                             <p className={joinStyles['textStyle']}>비밀번호</p>
                             <input id="password" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']} onChange={handleChange}/>
                         </div>
-                        <div>
+                        <div className={joinStyles['div']}>
                             <p className={joinStyles['textStyle']}>비밀번호 검사</p>
                             <input id="txtRePw" type="password" placeholder='비밀번호를 입력하시오.' className={joinStyles['inputStyles']} onChange={handleConfirmPasswordChange}/>
-                            {passwordMismatch && <p className={joinStyles['errorText']}>비밀번호가 일치하지 않습니다.</p>}
+                            {passwordMismatch && <p className={joinStyles['errorMessage']}>비밀번호가 일치하지 않습니다.</p>}
                         </div>
-                        <div>
+                        <div className={joinStyles['div']}>
                             <p className={joinStyles['textStyle']}>학생 정보</p>
                             <div className={joinStyles['studentDivStyle']}>
                                 <div> <input id='grade' type='text' placeholder='학년' className={joinStyles['studentInputStyles']} onChange={handleChange}/> </div>
@@ -117,7 +126,7 @@ function Join() {
                             </div>
                         </div>
                         <div className={joinStyles['buttonContainer']}>
-                            <button id='btnJoin' className={joinStyles['buttonStyles']} type='submit' onClick={validatePassword}>회원가입</button>
+                            <button id='btnJoin' className={joinStyles['buttonStyles']} type='submit'>회원가입</button>
                         </div>
                     </div>
                     <div className={joinStyles['miniTextStyles']}>
