46ef7e65739c92e35f0e847245d86625aa307cd2
boram065 <boram4784@gmail.com>
2024-06-13 10:08:37 +0900

    Fix: 채팅방 클릭 시 해당 채팅방 이전 대화 불러오기

--- src/components/chatting/ProducerItem.js
+++ src/components/chatting/ProducerItem.js
@@ -12,26 +12,24 @@ function ProducerItem({ onClick, userData, chatRoomId }) {
     };
 
     return (
-        <>
-            <div className={styles['container']} onClick={handleClick}>
-                <div className={styles['img']}>
-                    {
-                        userData.profileUrl ? (
-                            <img src={userData.profileUrl} alt='profileImage'/>
-                        ) : (
-                            <img src='/images/profileImg.png' alt='profileImage'/>
-                        )
-                    }
-                </div>
-                <div className={styles['textDiv']}>
-                    <p>{userData.name}</p>
-                    {/* <div className={styles['text']}>
-                        <p>네 그럼 거래 가능합니다.</p>
-                        <p>2024-04-12</p>
-                    </div> */}
-                </div>
+        <div className={styles['container']} onClick={handleClick}>
+            <div className={styles['img']}>
+                {
+                    userData.profileUrl ? (
+                        <img src={userData.profileUrl} alt='profileImage'/>
+                    ) : (
+                        <img src='/images/profileImg.png' alt='profileImage'/>
+                    )
+                }
             </div>
-        </>
+            <div className={styles['textDiv']}>
+                <p>{userData.name}</p>
+                {/* <div className={styles['text']}>
+                    <p>네 그럼 거래 가능합니다.</p>
+                    <p>2024-04-12</p>
+                </div> */}
+            </div>
+        </div>
     )
 }
 
